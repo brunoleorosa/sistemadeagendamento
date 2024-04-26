36fcbd8f242c973efd5fe0f9b5963945b1216448
Bruno Leonardo <brunoleorosa@gmail.com>
2024-04-26 18:12:07 -0300

    Implementada a função de busca paciente.

--- src/main/java/br/com/clinicaacupuntura/sistemadeagendamento/repositories/PacienteRepository.java
+++ src/main/java/br/com/clinicaacupuntura/sistemadeagendamento/repositories/PacienteRepository.java
@@ -1,9 +1,19 @@
 package br.com.clinicaacupuntura.sistemadeagendamento.repositories;
 
 import br.com.clinicaacupuntura.sistemadeagendamento.entities.Paciente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PacienteRepository extends CrudRepository<Paciente, Long> {
+import java.util.List;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 
     Long countById(Long id);
+
+    @Query("SELECT p FROM Paciente p WHERE p.nome LIKE %?1%"
+            + " OR CONCAT('%', p.numeroDeInscricao) LIKE %?1%"
+            + " OR p.convenio LIKE %?1%")
+    List<Paciente> findAll(String keyword);
 }
